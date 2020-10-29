686798678848afc1ef7d72f70fd4a5823a144626
Jennifer Popp <jenpopp@flc25.desy.de>
2020-10-29 13:57:41 +0100

    update Analysis.hh

--- leap_sims_from_basic/include/Analysis.hh
+++ leap_sims_from_basic/include/Analysis.hh
@@ -27,8 +27,8 @@
 /// \file B4Analysis.hh
 /// \brief Selection of the analysis technology
 
-#ifndef B4Analysis_h
-#define B4Analysis_h 1
+#ifndef Analysis_h
+#define Analysis_h 1
 
 #include "g4root.hh"
 //#include "g4csv.hh"
